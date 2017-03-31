22a97ed42c822af617e5a2e721f835e2a9e7229c
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2017-03-31 11:26:32 +0200

    implement classloaders

--- src/jus/aor/mobilagent/kernel/Agent.java
+++ src/jus/aor/mobilagent/kernel/Agent.java
@@ -1,9 +1,16 @@
 package jus.aor.mobilagent.kernel;
 
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URI;
+import java.util.logging.Level;
+
 public class Agent implements _Agent{
 	
 	private AgentServer server;
 	private Route route;
+	private String serv_name;
 
 	protected _Action retour() {
 		// TODO Auto-generated method stub
@@ -32,8 +39,28 @@ public class Agent implements _Agent{
 
 	@Override
 	public void addEtape(Etape etape) {
+<<<<<<< HEAD
 		// TODO Auto-generated method stub
 		this.route.add(etape);
+=======
+		this.route.add(etape);
+	}
+	
+	public void goNext (){
+		URI destination = this.route.get().server;
+		try {
+			// nouveau socket de destination
+			Socket destinationSocket = new Socket(destination.getHost(), destination.getPort());
+			// récupération de l'output stream de destination
+			OutputStream out = destinationSocket.getOutputStream();
+			// création de l'objet à transmettre sur l'output stream créé au dessus
+			ObjectOutputStream objet = new ObjectOutputStream(out);
+			// récupération de la classe (code)
+			BAMAgentClassLoader classLoader = (BAMAgentClassLoader) this.getClass().getClassLoader();
+		} catch (Exception e){
+			System.out.println(e);
+		}
+>>>>>>> implement classloaders
 	}
 
 }
