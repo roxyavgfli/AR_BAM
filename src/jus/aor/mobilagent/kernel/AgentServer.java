8b401b2c6028254bc5f4b47772062b9ba6a11488
Stelle <estelle.allard2@gmail.com>
2017-04-04 18:54:34 +0200

    objectif 3, marche t-il ? suspence ...

--- src/jus/aor/mobilagent/kernel/AgentServer.java
+++ src/jus/aor/mobilagent/kernel/AgentServer.java
@@ -1,10 +1,13 @@
 package jus.aor.mobilagent.kernel;
 
+import java.net.ServerSocker;
+
 public class AgentServer extends Thread{
 
 	String name;
 	int port;
 	_Service<T> service;
+	ServerSocket servListener;
 	
 	
 	public AgentServer(String name, int port) {
@@ -13,8 +16,46 @@ public class AgentServer extends Thread{
 	}
 	
 	public void run(){ 
-		//TODO
+		System.out.println("Run agent serveur");
+		Jar jar;
+		Socket socketClient;
+		_Agent ag;
 		
+		try {
+			servListener = new ServerSocket(port);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		
+		
+		while(true){
+			try {
+				socketClient = servListener.accept();
+				ObjectInputStream ois = new ObjectInputStream(socketClient.getInputStream());
+				jar = (Jar) ois.readObject();
+				BAMAgentClassLoader BAMAgent = new BAMAgentClassLoader(this.getClass().getClassLoader());
+				BAMAgent.integrateCode(jar);
+				AgentInputStream ais = new AgentInputStream(socketClient.getInputStream(),BAMAgent);
+				ag = (_Agent) ais.readObject();
+				ais.close();
+				ag.reInit(this, this.name, BAMAgent);
+				Thread t = new Thread(ag);
+				t.start();
+
+				
+			}catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 	}
 	
 	private _Agent getAgent (Socket sock){
